9037ab0cde8e33a975c9ed1f4f101b3bf29f9511
missy65 <missyhaines46@gmail.com>
2025-07-12 08:44:59 -0500

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,8 +3,7 @@
 const chalk = require('chalk');
 
 module.exports = {
-  info: (...args) => console.log(chalk.cyan('[INFO]'), ...args),
-  success: (...args) => console.log(chalk.green('[SUCCESS]'), ...args),
-  error: (...args) => console.error(chalk.red('[ERROR]'), ...args),
-  warn: (...args) => console.warn(chalk.yellow('[WARN]'), ...args),
+  info: (msg) => console.log(chalk.blueBright('[INFO]'), msg),
+  success: (msg) => console.log(chalk.green('[SUCCESS]'), msg),
+  error: (msg) => console.error(chalk.red('[ERROR]'), msg),
 };
